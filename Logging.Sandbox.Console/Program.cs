661ab72f8155cc442e36df7503894093d21c00bf
Jim B <jbanning1981@users.noreply.github.com>
2023-12-09 12:25:07 -0600

    Configured logger and added simple debug scenario

--- Logging.Sandbox.Console/Program.cs
+++ Logging.Sandbox.Console/Program.cs
@@ -1,19 +1,38 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Microsoft.Extensions.Configuration;
-
+using Serilog;
+using Serilog.Settings.Configuration;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
+
         Console.WriteLine("Hello, World!");
 
+        var config = BuildConfiguration();
+
+        var logger = new LoggerConfiguration()
+                        .ReadFrom.Configuration(config)
+                        .CreateLogger();
+
+        try
+        {
 
 
+            logger.Information($"Starting sandbox at {DateTimeOffset.Now.ToString()}");
+
+            logger.Warning("Throwing Exception now...");
+            throw new ApplicationException("This exception should be caught and logged.");
+        }
+        catch (Exception ex)
+        {
+            logger.Error(ex, $"An exception occurred while running the app : {ex.Message}");
+        }
 
     }
 
-    private IConfiguration BuildConfiguration()
+    private static IConfiguration BuildConfiguration()
     {
 
         var envJsonFile = $"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Development"}.json";
