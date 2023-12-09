76aa86ae17e18f177ab7f7fe249b7ab6d0de9a61
Jim B <jbanning1981@users.noreply.github.com>
2023-12-09 10:54:21 -0600

    Create the intial project

---
+++ Logging.Sandbox.Console/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.Extensions.Configuration;
+
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+
+
+
+    }
+
+    private IConfiguration BuildConfiguration()
+    {
+
+        var envJsonFile = $"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Development"}.json";
+        IConfigurationBuilder builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .AddJsonFile(envJsonFile, false)
+            .AddEnvironmentVariables();
+
+
+        return builder.Build();
+
+
+    }
+}
